d4b6c904dd988aa0b5f6bcdabec171b921f8d078
Christian Weiske <cweiske@php.net>
2006-11-26 22:22:33 +0000

    Forgot to commit this file
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Crypt/trunk@223937 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/html-nojavascript.php
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+  <title>HTML_Crypt example without javascript</title>
+ </head>
+ <body>
+  <p>
+   Deactivating javascript output in HTML_Crypt
+  </p>
+  <p>
+<?php
+require_once 'HTML/Crypt.php';
+
+$c = new HTML_Crypt('yourname@example.com', 8, false);
+$c->addMailTo();
+$c->output();
+?>
+  </p>
+ </body>
+</html>
